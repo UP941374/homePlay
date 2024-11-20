d17b92d3a446e350e00ad423abefbfe25a588d43
Arek Uchymiak <auser93@gmail.com>
2024-11-20 08:59:15 +0000

    movie player set up

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3075;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
